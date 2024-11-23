9d88ecb2aef1112d437f5ad467e10c215b043df4
Arpit Sharma <145807799+arpitsharma6074@users.noreply.github.com>
2024-11-23 10:32:13 +0530

    Updated Project (both)

---
+++ Expense_management_project/src/main/java/com/example/demo/service/UserExpenseService.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Expense;
+
+
+import java.util.List;
+
+public interface UserExpenseService {
+    public String createExpense(Expense expanse );
+
+    public List<Expense> getAllExpense();
+
+    String removeUserExpense(Long expenseId);
+}
