0195b99663f7bbedefa02878b015f1ca311b5d84
Arpit Sharma <145807799+arpitsharma6074@users.noreply.github.com>
2024-12-03 22:40:35 +0530

    Delete Expense_management_project/src/main directory

--- Expense_management_project/src/main/java/com/example/demo/repository/UserExpenseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Expense;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserExpenseRepository extends JpaRepository<Expense,Long> {
-}
--- Expense_management_project/src/main/java/com/example/demo/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.model.Admin;
-import com.example.demo.model.LoginRequest;
-
-public interface AdminService {
-    public String createAdmin(Admin admin);
-    public Admin adminLogin(LoginRequest loginRequest);
-}
